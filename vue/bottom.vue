eda408f1853498b2f3fea742390c93b77fe6f686
anatomas <anatomas@mvarc.eu>
2025-12-09 15:17:45 +0000

    improve funding

--- vue/bottom.vue
+++ vue/bottom.vue
@@ -11,5 +11,15 @@ module.exports = {
 </script>
 
 <template>
-	<div class="footer fixed-bottom d-print-none"><p>This tool was developed under the EU project <a href="https://digitaf.eu/" target="_blank">DigitAF</a> (grant agreement 101059794), as part of Deliverable 4.5, and is intended to evolve according to needs.</p></div>
+  
+	<div class="footer fixed-bottom d-print-none">
+    <div class="row">
+      <div class="col-1 d-flex align-items-center justify-content-end text-right">
+        <img src="img/eu-flag.svg" alt="EU Flag" style="height: 100%; max-height: 20px; width: auto;">
+      </div>
+      <div class="col-11  d-flex align-items-center text-left">
+        <p class="m-0 small">This tool was developed under the EU project <a href="https://digitaf.eu/" target="_blank">DigitAF</a> (Grant Agreement N° 101059794), co-funded by the European Commission, European Research Agency, within the Horizon Europe programme, Cluster 6: “Food, bioeconomy, natural resources, agriculture and environment”, as part of Deliverable 4.5, and is intended to evolve according to needs.</p>
+      </div>
+    </div>
+  </div>
 </template>
